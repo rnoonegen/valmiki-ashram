9a7cf42f4344553aae4561c7c6735f9a8e36ac37
vengaldasuNagaraju <vengaldasunagaraju14@gmail.com>
2026-04-08 12:27:54 +0530

    i add ed the dummy data

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,26 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import AdminRoute from '../components/admin/AdminRoute';
-import Layout from '../components/Layout';
-import About from '../pages/About';
-import Admission from '../pages/Admission';
-import AdminLogin from '../pages/AdminLogin';
-import ContactUs from '../pages/ContactUs';
-import Contests from '../pages/Contests';
-import Curriculam from '../pages/Curriculam';
-import FAQ from '../pages/FAQ';
-import Founders from '../pages/Founders';
-import Gallery from '../pages/Gallery';
-import Gurukulam from '../pages/Gurukulam';
-import Home from '../pages/Home';
-import OnlineCourseRegistration from '../pages/OnlineCourseRegistration';
-import OnlinePrograms from '../pages/OnlinePrograms';
-import Programs from '../pages/Programs';
-import SummerCampRegistration from '../pages/SummerCampRegistration';
-import SummerCamp from '../pages/SummerCamp';
-import Terms from '../pages/Terms';
-import ValmikiAshram from '../pages/ValmikiAshram';
-import WinterCampRegistration from '../pages/WinterCampRegistration';
-import WinterCamp from '../pages/WinterCamp';
+import { Route, Routes } from "react-router-dom";
+import AdminRoute from "../components/admin/AdminRoute";
+import Layout from "../components/Layout";
+import About from "../pages/About";
+import Admission from "../pages/Admission";
+import AdminLogin from "../pages/AdminLogin";
+import ContactUs from "../pages/ContactUs";
+import Contests from "../pages/Contests";
+import Curriculam from "../pages/Curriculam";
+import FAQ from "../pages/FAQ";
+import Founders from "../pages/Founders";
+import Gallery from "../pages/Gallery";
+import Gurukulam from "../pages/Gurukulam";
+import Home from "../pages/Home";
+import OnlineCourseRegistration from "../pages/OnlineCourseRegistration";
+import OnlinePrograms from "../pages/OnlinePrograms";
+import Programs from "../pages/Programs";
+import SummerCampRegistration from "../pages/SummerCampRegistration";
+import SummerCamp from "../pages/SummerCamp";
+import Terms from "../pages/Terms";
+import ValmikiAshram from "../pages/ValmikiAshram";
+import WinterCampRegistration from "../pages/WinterCampRegistration";
+import WinterCamp from "../pages/WinterCamp";
 
 export default function AppRoutes() {
   return (
@@ -33,11 +33,20 @@ export default function AppRoutes() {
         <Route path="gallery" element={<Gallery />} />
         <Route path="programs" element={<Programs />} />
         <Route path="winter-camp" element={<WinterCamp />} />
-        <Route path="register/winter-camp" element={<WinterCampRegistration />} />
+        <Route
+          path="register/winter-camp"
+          element={<WinterCampRegistration />}
+        />
         <Route path="summer-camp" element={<SummerCamp />} />
-        <Route path="register/summer-camp" element={<SummerCampRegistration />} />
+        <Route
+          path="register/summer-camp"
+          element={<SummerCampRegistration />}
+        />
         <Route path="online-programs" element={<OnlinePrograms />} />
-        <Route path="register/online-course" element={<OnlineCourseRegistration />} />
+        <Route
+          path="register/online-course"
+          element={<OnlineCourseRegistration />}
+        />
         <Route path="gurukulam" element={<Gurukulam />} />
         <Route path="admission" element={<Admission />} />
         <Route path="contests" element={<Contests />} />
@@ -45,6 +54,7 @@ export default function AppRoutes() {
         <Route path="contact" element={<ContactUs />} />
         <Route path="faq" element={<FAQ />} />
         <Route path="terms" element={<Terms />} />
+        {/* <Route path="nri-summer" element={<NRI Summer Camp />} /> */}
       </Route>
       <Route path="admin-login" element={<AdminLogin />} />
       <Route element={<AdminRoute />}>
